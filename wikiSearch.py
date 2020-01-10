16c127f700c0946c2542ccb233c89d253e444944
Vishal Bhola <38592928+VersatileVishal@users.noreply.github.com>
2020-01-10 23:35:08 +0530

    Update wikiSearch.py

--- wikiSearch.py
+++ wikiSearch.py
@@ -1,1 +1,25 @@
 import wikipedia
+from tkinter import *
+from tkinter.messagebox import showinfo
+
+win = Tk()
+win.title('Wikipedia')
+win.geometry('500x150')
+
+def search_wiki():
+    search = entry.get()
+    answer = wikipedia.summary(search)
+    showinfo("Wikipedia Answer",answer)
+
+
+label = Label(win,text="Wikipedia Search :")
+label.grid(row=1, column=2)
+
+entry = Entry(win,width=25)
+entry.grid(row=3, column=6)
+
+button = Button(win,text="Search",command=search_wiki)
+button.grid(row=7,column=6,padx=10)
+
+
+win.mainloop()
